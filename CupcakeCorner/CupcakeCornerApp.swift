95a8e73c3327cd1e157fa0036f262612ef6a6f83
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-11-06 01:58:42 +0100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Zaid Neurothrone on 2022-11-06.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
